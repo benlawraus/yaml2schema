168fe76c21d84fd31f501cf9151052d26b7a04d7
ben <benlawraus@gmail.com>
2022-04-01 10:34:42 -0700

    open-ended yaml entry

--- src/yaml2schema/y2s_schema.py
+++ src/yaml2schema/y2s_schema.py
@@ -49,7 +49,35 @@ def openapi_preamble_schema():
     })
 
 
-def anvil_yaml_schema() -> sy.Map:
+# def anvil_yaml_schema() -> sy.Map:
+#     """
+#     Generates a strictyaml schema
+#
+#     Returns
+#     -------
+#         Schema object
+#     """
+#     # schema used by strictyaml to parse the text
+#     schema = sy.Map({
+#         'db_schema': sy.MapPattern(
+#             sy.Str(),
+#             sy.Map({
+#                 'title': sy.Str(),
+#                 'client': sy.Str(),
+#                 'server': sy.Str(),
+#                 'columns': sy.Seq(sy.Map({
+#                     'name': sy.Str(),
+#                     'admin_ui': sy.Any(),
+#                     'type': sy.Str(),
+#                     sy.Optional('target'): sy.Str()
+#                 }))
+#             })
+#         )
+#     })
+#     # anvil.yaml uses 'flow style' in certain places.
+#     return schema
+
+def anvil_yaml_schema() -> sy.MapPattern:
     """
     Generates a strictyaml schema
 
@@ -58,21 +86,5 @@ def anvil_yaml_schema() -> sy.Map:
         Schema object
     """
     # schema used by strictyaml to parse the text
-    schema = sy.Map({
-        'db_schema': sy.MapPattern(
-            sy.Str(),
-            sy.Map({
-                'title': sy.Str(),
-                'client': sy.Str(),
-                'server': sy.Str(),
-                'columns': sy.Seq(sy.Map({
-                    'name': sy.Str(),
-                    'admin_ui': sy.Any(),
-                    'type': sy.Str(),
-                    sy.Optional('target'): sy.Str()
-                }))
-            })
-        )
-    })
-    # anvil.yaml uses 'flow style' in certain places.
+    schema = sy.MapPattern(sy.Str(), sy.Any())
     return schema
